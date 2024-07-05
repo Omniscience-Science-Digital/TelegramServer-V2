6dc5a6244ba201e0b1aa4db2335e3f8d51c227a6
ThabisoBlessed <thabiso@omniscience.digital>
2024-07-05 12:37:45 +0200

    Caddy server setting

--- controllers/mqtt.controller.js
+++ controllers/mqtt.controller.js
@@ -30,6 +30,8 @@ exports.Mqttcontroller = async (req, res) => {
 
         let combinedResult = [];
 
+
+
         // Loop through each row in csvData (excluding headers)
         for (let i = 1; i < csvData.length; i++) {
 
@@ -59,7 +61,7 @@ exports.Mqttcontroller = async (req, res) => {
                     }
 
                     date = row[1];
-
+                    
                     const adValue = adMapping[key];
                     const columnIndex = headerIndexMap[key];
 
@@ -77,11 +79,8 @@ exports.Mqttcontroller = async (req, res) => {
 
 
 
-            if (date) {
-                let isoDate = new Date(date).toISOString();
-                jsonResult['date'] = isoDate;
-            }
-
+            jsonResult['date'] = parseDate(date);
+            
 
             let databaseJson = JSON.stringify(jsonResult);
 
@@ -92,11 +91,12 @@ exports.Mqttcontroller = async (req, res) => {
 
 
         try {
-            
-            // console.log(combinedResult)
+
+           // console.log(iccid)
+             //console.log(combinedResult)
             // Publish MQTT message
             // await processData(combinedResult, '7082229037010123040');
-            await processData(combinedResult, iccid);
+           await processData(combinedResult, iccid);
         } catch (error) {
             console.error(`Error publishing MQTT message: ${error}`);
             throw error; // Optionally rethrow to propagate the error further
@@ -109,4 +109,21 @@ exports.Mqttcontroller = async (req, res) => {
         console.error('Error in Mqttcontroller:', error);
         res.status(500).send('Internal server error');
     }
+}
+
+
+function parseDate(dateString) {
+    // Check if the date string is in the format "YYYY/MM/DD HH:MM"
+    let isoDate;
+    if (/\d{4}\/\d{2}\/\d{2} \d{2}:\d{2}/.test(dateString)) {
+        isoDate = new Date(dateString.replace(/\//g, '-')).toISOString();
+    } else if (/\d{2}\/\d{2}\/\d{4} \d{2}:\d{2}/.test(dateString)) {
+        // If the date is in the format "DD/MM/YYYY HH:MM"
+        let parts = dateString.split(/[\s/:]+/);
+        isoDate = new Date(parts[2], parts[1] - 1, parts[0], parts[3], parts[4]).toISOString();
+    } else {
+        throw new Error("Unsupported date format");
+    }
+
+    return isoDate;
 }
